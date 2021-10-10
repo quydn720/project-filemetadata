4e4fb3fbf581bc49d28750a107be64cafefb49ac
Ngọc Quý Đỗ <quydn720@gmail.com>
2021-10-10 15:21:02 +0700

    Solution

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+let fs = require('fs-extra');
 
 var app = express();
+let upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, callback) => {
+      let type = req.params.type;
+      let path = `./uploads/${type}`;
+      fs.mkdirsSync(path);
+      callback(null, path);
+    },
+    filename: (req, file, callback) => {
+      callback(null, file.originalname);
+    }
+  })
+})  
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +26,15 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',upload.single("upfile"), function(req, res, next){
+  const {filename, mimetype, size} = req.file;
+  res.json({
+    name: filename,
+    type: mimetype,
+    size: size,
+  })
+  res.send();
+});
 
 
 
